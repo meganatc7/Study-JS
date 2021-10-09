b860afded04a262f218523c0457601c4669b37dd
meganatc7 <meganatc7@gmail.com>
2021-10-09 15:08:36 +0900

    Add Horizonta Tab

---
+++ HTML&CSS&JavaScript/Horizontal Tab/script.js
@@ -0,0 +1,16 @@
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    btns.forEach((btn) => btn.classList.remove("active"));
+
+    btn.classList.add("active");
+    const tabContents = document.querySelectorAll(".tab-content");
+    tabContents.forEach((tabContent) => {
+      tabContent.className = tabContent.className.replace("active", "");
+    });
+    // tabContents.forEach((tab) => tab.classList.remove("active")); 위 replace와 같은 코드!!
+    document.querySelector(btn.dataset.targetTab).classList.add("active");
+    console.log(btn.dataset.targetTab);
+  });
+});
