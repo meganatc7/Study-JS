d8e82d78809c750e563b7f149f7772309780b884
meganatc7 <meganatc7@gmail.com>
2021-11-20 17:29:34 +0900

    Add react prac1

---
+++ React/prac1/src/containers/whatGPT3/WhatGPT3.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Feature } from "../../components";
+import "./whatGPT3.css";
+
+const WhatGPT3 = () => {
+  const text = {
+    first:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius dignissimos doloremque laboriosam quae, voluptatem soluta labore aspernatur. Vero, nihil a repellat accusantium cumque quod, officiis similique neque placeat alias necessitatibus!",
+    second:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum saepe amet veritatis eos eius tempora",
+    third: "Lorem ipsum dolor sit amet consectetur adipisicing elit. tempora",
+    forth:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum saepe amet veritatis eos eius tempora",
+  };
+
+  return (
+    <div className="gpt3__whatgpt3 section__margin" id="whpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature title="What is CPT-3" text={text.first} />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">
+          Possibility are beyond your imagination
+        </h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature title="Chatbots" text={text.second} />
+        <Feature title="Knowledgebase" text={text.third} />
+        <Feature title="Education" text={text.forth} />
+      </div>
+    </div>
+  );
+};
+
+export default WhatGPT3;
