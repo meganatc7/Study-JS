8f064a581cd9d55acc3655633b1eb39378c21e1e
meganatc7 <meganatc7@gmail.com>
2022-02-12 16:56:16 +0900

    Add study next

--- React/prac2/pages/api/products/index.js
+++ React/prac2/pages/api/products/index.js
@@ -10,7 +10,10 @@ export default async function handler(req, res) {
   }
   if (method === "POST") {
     try {
+      console.log('firsthi')
       const product = await Product.create(req.body);
+      console.log("hihi")
+      console.log(product)
       res.status(201).json(product);
     } catch (err) {
       res.status(500).json(err);
